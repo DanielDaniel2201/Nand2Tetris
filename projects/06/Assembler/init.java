ae30ab718ef211cfb0664a76db129327634969e1
Yuanqiu Wang <123370445+DanielDaniel2201@users.noreply.github.com>
2023-12-04 16:47:40 +0800

    Update fiie path

--- projects/06/Assembler/init.java
+++ projects/06/Assembler/init.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public class init {
     public static void main(String[] args) throws IOException {
-        String FilePath1 = "D://courses//CS_courses//nand2tetris//projects//06//max//MaxL.asm";
+        String FilePath1 = "../../max/MaxL.asm";
         String OutFile = "MaxLOut.txt";
-        String FilePath2 = "D://courses//CS_courses//nand2tetris//projects//06//pong//Pong.asm";
+        String FilePath2 = "../../pong/Pong.asm";
 
         // BasicHackAssembler basicHackAssembler = new BasicHackAssembler(FilePath1, OutFile);
         // basicHackAssembler.assemble();
